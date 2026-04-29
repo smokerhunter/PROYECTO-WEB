104021b2055abb81be7f70f9108f431fdce2af50
ryan32312 <smokerporsiempre@gmail.com>
2026-04-29 14:35:10 -0500

    Modificando y aumentado js

---
+++ JS/script.js
@@ -0,0 +1,46 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const btnTop = document.getElementById("btnTop");
+
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > 300) {
+            btnTop.style.display = "block";
+        } else {
+            btnTop.style.display = "none";
+        }
+    });
+
+    btnTop.addEventListener("click", () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    });
+
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const btn = document.getElementById("btnBienvenida");
+
+    // aparece después de 2 segundos
+    setTimeout(() => {
+        btn.style.display = "block";
+    }, 2000);
+
+    // mensaje al hacer click
+    btn.addEventListener("click", () => {
+        alert("Bienvenido a mi página web 😎✨");
+    });
+
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const btnChat = document.getElementById("btnChat");
+
+    btnChat.addEventListener("click", () => {
+        alert("💬 Contáctame — chat abierto 24/7 😎");
+    });
+
+});
